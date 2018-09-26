1b2f090a9e281ee1ccb00ca05359977a22eb32b9
Athos <z5134298@ad.unsw.edu.au>
2018-09-26 19:12:21 +1000

    torch light

---
+++ UNSWgraph-0.18/shaders/fragment_tex_phong_flashlight.glsl
@@ -0,0 +1,93 @@
+
+out vec4 outputColor;
+
+uniform vec4 input_color;
+
+uniform mat4 view_matrix;
+
+// -------------------------
+// our codes
+// Light properties
+// light position is just camera position
+uniform vec3 lightPos;
+uniform vec3 lightIntensity;
+uniform vec3 ambientIntensity;
+// -------------------------
+
+// Material properties
+uniform vec3 ambientCoeff;
+uniform vec3 diffuseCoeff;
+uniform vec3 specularCoeff;
+uniform float phongExp;
+
+uniform sampler2D tex;
+
+in vec4 viewPosition;
+in vec3 m;
+
+in vec2 texCoordFrag
+
+// -------------------------
+// our codes
+in vec3 normal_light;
+
+uniform float cutOff;
+
+// distance formular factors
+uniform float constant
+uniform float linear
+uniform float quadratic
+// -------------------------
+
+
+void main()
+{
+    // Compute the s, v and r vectors
+    vec3 s = normalize(view_matrix*vec4(lightPos,1) - viewPosition).xyz;
+    vec3 v = normalize(-viewPosition.xyz);
+    vec3 r = normalize(reflect(-s,m));
+
+    // ------------------------
+    // our code
+    vec3 temp_vec3 = ( view_matrix*vec4(lightPos,1) - viewPosition ).xyz;
+    float distance = length( temp_vec3 );
+    float attenuation = 1.0 / ( constant + linear * distance +
+    		    quadratic * (distance * distance));
+    // ------------------------
+
+    vec3 ambient = ambientIntensity*ambientCoeff;
+    vec3 diffuse = max(lightIntensity*diffuseCoeff*dot(m,s), 0.0);
+    vec3 specular;
+
+    // ------------------------
+    // our code
+    ambient = ambient * attenuation;
+    diffuse = diffuse * attenuation;
+    // ------------------------
+
+    // Only show specular reflections for the front face
+    if (dot(m,s) > 0)
+        specular = max(lightIntensity*specularCoeff*pow(dot(r,v),phongExp), 0.0);
+    else
+        specular = vec3(0);
+
+    // ------------------------
+    // our code
+    specular = specular * attenuation;
+    // ------------------------
+
+    vec4 ambientAndDiffuse = vec4(ambient + diffuse, 1);
+
+
+    // ------------------------
+    // our codes
+    vec3  norm_temp = normalize( normal_light )
+
+    if ( dot( norm_temp , s ) <= cutOff )
+      outputColor = ambientAndDiffuse*input_color*texture(tex, texCoordFrag) + vec4(specular, 1);
+    else
+      vec4 dark_ambientAndDiffuse = vec4( 0.2 , 0.2 , 0.2 , 1 );
+      outputColor = dark_ambientAndDiffuse*input_color*texture(tex, texCoordFrag) + vec4(specular, 1);
+    // -------------------------
+
+}
